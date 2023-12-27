5be58e37dbdd93ea43800057d75642eaeb5d7d56
Henrik1995a@live.se <henrik1995a@live.se>
2023-12-27 14:17:37 +0100

    Revert "Update passwordHash"
    This reverts commit f4c829a993e87a76a3bf7516c8cbd421dee913a7.

--- HealthCare.Core/Models/User/User.cs
+++ HealthCare.Core/Models/User/User.cs
@@ -9,7 +9,6 @@ namespace HealthCare.Core.Models.User
     public abstract class User
     {
         public int Id { get; set; }
-        public string PasswordHash { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
