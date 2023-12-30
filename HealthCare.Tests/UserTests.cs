3e02187419582f8acdef4a8469b7f1421a1ac5bd
Henrik1995a@live.se <henrik1995a@live.se>
2023-12-30 12:43:39 +0100

    Added tests for login procedure

--- HealthCare.Tests/UserTests.cs
+++ HealthCare.Tests/UserTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthCare.WebApp.Pages.Authentication;
 
 namespace HealthCare.Tests
 {
@@ -25,7 +26,7 @@ namespace HealthCare.Tests
         {
             //Arrange
             //Note that user has no code to be doctor.
-            var userForTest = new RegisterDto("Test", "email", "firstname", "lastname", GenderEnum.Male, DateTime.Now);
+            var userForTest = new RegisterDto("Test", "Password", "firstname", "lastname", GenderEnum.Male, DateTime.Now);
 
             //Act
             var result = _authService.RegisterUser(userForTest);
@@ -40,7 +41,7 @@ namespace HealthCare.Tests
             //Arrange
             //Note that this user has a code to be a doctor.
             //Also note that we need to use a different email from first test.
-            var userForTest = new RegisterDto("Test", "email123", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
+            var userForTest = new RegisterDto("Test", "Password", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
 
             //Act
             var result = _authService.RegisterUser(userForTest);
@@ -54,10 +55,10 @@ namespace HealthCare.Tests
         {
             //Arrange
             //Adding user with same email 2 times. Resulting in error.
-            var userForTest1 = new RegisterDto("Test", "TestEmail", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
-            var userForTest2 = new RegisterDto("Test", "TestEmail", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
-            //Act
+            var userForTest1 = new RegisterDto("Test", "Password", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
+            var userForTest2 = new RegisterDto("Test", "Password", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
 
+            //Act
             var result = _authService.RegisterUser(userForTest1);
             var result2 = _authService.RegisterUser(userForTest2);
 
@@ -68,6 +69,33 @@ namespace HealthCare.Tests
             //Error result since user already exists.
             Assert.NotNull(result2);
             Assert.Equal("Error registering, please try again", result2);
+        }
+        [Fact]
+        public void Login_User_Should_Return_User()
+        {
+            //Arrange
+            var userToRegister = new RegisterDto("Test", "Password", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
+
+            //Act
+            _authService.RegisterUser(userToRegister);
+            var resultFromLogin = _authService.Authenticate(new LoginDto(userToRegister.Email, userToRegister.Password));
+
+            //Assert
+            Assert.NotNull(resultFromLogin);
+            Assert.Equal(userToRegister.Email, resultFromLogin.Email);
+        }
+        [Fact]
+        public void Login_User_Wrong_Password_Should_Return_Null()
+        {
+            //Arrange
+            var userToRegister = new RegisterDto("Test", "Password", "firstname", "lastname", GenderEnum.Male, DateTime.Now, "123");
+
+            //Act
+            _authService.RegisterUser(userToRegister);
+            var resultFromLogin = _authService.Authenticate(new LoginDto(userToRegister.Email, "WrongPass"));
+
+            //Assert
+            Assert.Null(resultFromLogin);
         }
     }
 }
