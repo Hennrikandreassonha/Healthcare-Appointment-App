3e6944ce93ecdc1027d7c696a446f8853a430668
Magnus Könberg <konbergm73@gmail.com>
2024-01-09 16:20:41 +0100

    fixed sorting times for patient

--- HealthCare.Core/AppointmentService.cs
+++ HealthCare.Core/AppointmentService.cs
@@ -130,7 +130,7 @@ namespace HealthCare.Core
         public IEnumerable<Appointment> GetInitialAppointmentsByDate(DateTime date)
         {
             return _context.Appointment.Where(x => x.DateTime.Year == date.Year && x.DateTime.DayOfYear == date.DayOfYear && x.PatientId == null)
-                .Include(x => x.CareGiver).ToList();
+                .Include(x => x.CareGiver).OrderBy(a => a.DateTime.Hour).ToList();
         }
         public bool RemoveInitialAppointmentsForADay(int caregiverId, DateTime date)
         {
