e615d105c1ac87d97e689e6d46a24826d94d520f
Nikell97 <nikellmattias@gmail.com>
2024-01-11 09:22:13 +0100

    test fixes and updated db model

---
+++ HealthCare.Core/Models/UserModels/MedicalJournalEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCare.Core.Models.UserModels
+{
+    public class MedicalJournalEntry
+    {
+        public int Id { get; set; }
+
+        public string Entry {  get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public Patient Patient { get; set; }
+
+        public CareGiver CareGiver { get; set; }
+    }
+}
