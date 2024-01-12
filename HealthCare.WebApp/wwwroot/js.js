5501c229455d1c364945b16335eea6f438826b32
Henrik1995a@live.se <henrik1995a@live.se>
2024-01-12 11:00:18 +0100

    chatbot update

--- HealthCare.WebApp/wwwroot/js.js
+++ HealthCare.WebApp/wwwroot/js.js
@@ -9,3 +9,20 @@ function SelectElementFix() {
         element.selectedIndex = 0;
     });
 }
+
+//For the chatbot.
+
+function ShowChatPopup(){
+    const chatpopUp = document.getElementById("chat-main-div");
+    chatpopUp.classList.remove('display-none');
+    chatpopUp.classList.add('display-block');
+}
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.add("show");
+}
+
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.remove("show");
+}
