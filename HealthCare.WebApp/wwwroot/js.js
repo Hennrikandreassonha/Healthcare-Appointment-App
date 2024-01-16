7468be9e202c345be2232ced90e8fece17a9207c
Henrik1995a@live.se <henrik1995a@live.se>
2024-01-16 08:50:42 +0100

    Enter to send message

--- HealthCare.WebApp/wwwroot/js.js
+++ HealthCare.WebApp/wwwroot/js.js
@@ -25,3 +25,21 @@ function scrollToBottom() {
     var chatContainer = document.getElementById('chat-div');
     chatContainer.scrollTop = chatContainer.scrollHeight;
 }
+function enterToSendMessage() {
+    var chatContainer = document.getElementById("question-area");
+    chatContainer.addEventListener("keydown", submitOnEnter);
+}
+let testObj;
+setDotnetObject = function (dotNetObject) {
+    testObj = dotNetObject;
+};
+function submitOnEnter(event) {
+    if (event.which === 13 && !event.shiftKey) {
+        if (!event.repeat) {
+            var chatContainer = document.getElementById("question-area");
+            testObj.invokeMethodAsync('SendQuestionJS', chatContainer.value);
+            chatContainer.value = "";
+        }
+        event.preventDefault();
+    }
+}
